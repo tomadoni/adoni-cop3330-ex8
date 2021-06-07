40f1d1ddf8f565f9799c043c2be8c5a6c12dd6dc
Tom Adoni <tomad101@gmail.com>
2021-06-07 18:13:27 -0400

    Initial commit

---
+++ src/exercise8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main {
+
+    public static void main(String[] args) {
+        Scanner kb= new Scanner(System.in);
+        int people=0;
+        int pizza=0;
+        int slices=0;
+        System.out.println("How many people?");
+        people= kb.nextInt();
+        System.out.println("How many pizzas do you have?");
+        pizza= kb.nextInt();
+        System.out.println("How many slices per pizza?");
+        slices= kb.nextInt();
+        System.out.println(people+" people with "+pizza+" pizzas ("+(pizza*slices)+" slices)");
+        System.out.println("Each person gets "+(pizza*slices)/people+" pieces of pizza.");
+        System.out.println("There are "+(pizza*slices)%people+" leftover pieces.");
+    }
+}
